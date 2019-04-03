0b34be0dff0f6567e6b988f972352bad707217fb
Russell Gold <russell.gold@oracle.com>
2019-04-03 15:23:32 -0400

    support JDK11-style create class

--- orbmain/src/main/java/com/sun/corba/ee/impl/presentation/rmi/codegen/CodegenProxyCreator.java
+++ orbmain/src/main/java/com/sun/corba/ee/impl/presentation/rmi/codegen/CodegenProxyCreator.java
@@ -10,26 +10,48 @@
 
 package com.sun.corba.ee.impl.presentation.rmi.codegen;
 
-import org.glassfish.pfl.dynamic.codegen.spi.Primitives;
-import org.glassfish.pfl.dynamic.codegen.spi.Variable;
-import org.glassfish.pfl.dynamic.codegen.spi.Expression;
+import static java.lang.reflect.Modifier.ABSTRACT;
+import static java.lang.reflect.Modifier.PRIVATE;
+import static java.lang.reflect.Modifier.PUBLIC;
+import static org.glassfish.pfl.dynamic.codegen.spi.Wrapper.DUMP_AFTER_SETUP_VISITOR;
+import static org.glassfish.pfl.dynamic.codegen.spi.Wrapper.TRACE_BYTE_CODE_GENERATION;
+import static org.glassfish.pfl.dynamic.codegen.spi.Wrapper.USE_ASM_VERIFIER;
+import static org.glassfish.pfl.dynamic.codegen.spi.Wrapper._Object;
+import static org.glassfish.pfl.dynamic.codegen.spi.Wrapper._arg;
+import static org.glassfish.pfl.dynamic.codegen.spi.Wrapper._body;
+import static org.glassfish.pfl.dynamic.codegen.spi.Wrapper._call;
+import static org.glassfish.pfl.dynamic.codegen.spi.Wrapper._cast;
+import static org.glassfish.pfl.dynamic.codegen.spi.Wrapper._class;
+import static org.glassfish.pfl.dynamic.codegen.spi.Wrapper._clear;
+import static org.glassfish.pfl.dynamic.codegen.spi.Wrapper._const;
+import static org.glassfish.pfl.dynamic.codegen.spi.Wrapper._constructor;
+import static org.glassfish.pfl.dynamic.codegen.spi.Wrapper._define;
+import static org.glassfish.pfl.dynamic.codegen.spi.Wrapper._end;
+import static org.glassfish.pfl.dynamic.codegen.spi.Wrapper._expr;
+import static org.glassfish.pfl.dynamic.codegen.spi.Wrapper._generate;
+import static org.glassfish.pfl.dynamic.codegen.spi.Wrapper._method;
+import static org.glassfish.pfl.dynamic.codegen.spi.Wrapper._new_array_init;
+import static org.glassfish.pfl.dynamic.codegen.spi.Wrapper._package;
+import static org.glassfish.pfl.dynamic.codegen.spi.Wrapper._return;
+import static org.glassfish.pfl.dynamic.codegen.spi.Wrapper._setClassLoader;
+import static org.glassfish.pfl.dynamic.codegen.spi.Wrapper._super;
+import static org.glassfish.pfl.dynamic.codegen.spi.Wrapper._this;
+import static org.glassfish.pfl.dynamic.codegen.spi.Wrapper._void;
+import static org.glassfish.pfl.dynamic.codegen.spi.Wrapper.splitClassName;
+
+import java.io.PrintStream;
+import java.lang.reflect.Method;
+import java.security.ProtectionDomain;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 import org.glassfish.pfl.basic.contain.Pair;
-import org.glassfish.pfl.dynamic.codegen.spi.Utility;
+import org.glassfish.pfl.dynamic.codegen.spi.Expression;
 import org.glassfish.pfl.dynamic.codegen.spi.MethodInfo;
+import org.glassfish.pfl.dynamic.codegen.spi.Primitives;
 import org.glassfish.pfl.dynamic.codegen.spi.Type;
-import java.io.PrintStream ;
-
-import java.lang.reflect.Method ;
-
-import java.security.ProtectionDomain ;
-
-import java.util.Properties ;
-import java.util.List ;
-import java.util.ArrayList ;
-
-import static java.lang.reflect.Modifier.* ;
-
-import static org.glassfish.pfl.dynamic.codegen.spi.Wrapper.* ;
+import org.glassfish.pfl.dynamic.codegen.spi.Utility;
+import org.glassfish.pfl.dynamic.codegen.spi.Variable;
 
 /** Generate a proxy with a specified base class.  
  */
@@ -95,11 +117,13 @@ public class CodegenProxyCreator {
      * Note that the generated methods ignore exceptions.
      * It is assumed that the invoke method may throw any
      * desired exception.
-     * @param className the name of the generated class
-     * @param superClassName the name of the class extends by the generated class
-     * @param interfaces the interfaces implemented by the generated class
-     * @param methods the methods that the generated class implements
+     * @param pd the protection domain of the generated class
+     * @param cl the classloader in which to generate the class
+     * @param debug if true, generate debug messages
+     * @param ps a PrintStream to which the debug messages should be written
+     * @deprecated use {@link #create(Class, boolean, PrintStream)}
      */
+    @Deprecated
     public Class<?> create( ProtectionDomain pd, ClassLoader cl,
         boolean debug, PrintStream ps ) {
 
@@ -127,6 +151,70 @@ public class CodegenProxyCreator {
         _end() ; // of _class
 
         return _generate( cl, pd, debug ? debugProps : emptyProps, ps ) ;
+    }
+
+    /** Construct a generator for a proxy class
+     * that implements the given interfaces and extends superClass.
+     * superClass must satisfy the following requirements:
+     * <ol>
+     * <li>It must have an accessible no args constructor</li>
+     * <li>It must have a method satisfying the signature
+     * <code>    Object invoke( int methodNumber, Object[] args ) throws Throwable
+     * </code>
+     * </li>
+     * <li>The invoke method described above must be accessible
+     * to the generated class (generally either public or
+     * protected.</li>
+     * </ol>
+     * <p>
+     * Each method in methods is implemented by a method that:
+     * <ol>
+     * <li>Creates an array sized to hold the args</li>
+     * <li>Wraps args of primitive type in the appropriate wrapper.</li>
+     * <li>Copies each arg or wrapper arg into the array.</li>
+     * <li>Calls invoke with a method number corresponding to the
+     * index of the method in methods.  Note that the invoke implementation
+     * must use the same method array to figure out which method has been
+     * invoked.</li>
+     * <li>Return the result (if any), extracting values from wrappers
+     * as needed to handle a return value of a primitive type.</li>
+     * </ol>
+     * <p>
+     * Note that the generated methods ignore exceptions.
+     * It is assumed that the invoke method may throw any
+     * desired exception.
+     * @param anchorClass a class in whose classloader the new class should be generated
+     * @param debug if true, generate debug messages
+     * @param ps a PrintStream to which the debug messages should be written
+     * @since 4.2.1
+     */
+    public Class<?> create( Class<?> anchorClass,
+        boolean debug, PrintStream ps ) {
+
+        Pair<String,String> nm = splitClassName( className ) ;
+
+        _clear() ;
+        _setClassLoader( anchorClass.getClassLoader() ) ;
+        _package( nm.first() ) ;
+        _class( PUBLIC, nm.second(), superClass, interfaces ) ;
+
+        _constructor( PUBLIC ) ;
+        _body() ;
+            _expr(_super());
+        _end() ;
+
+        _method( PRIVATE, _Object(), "writeReplace" ) ;
+        _body() ;
+            _return(_call(_this(), "selfAsBaseClass" )) ;
+        _end() ;
+
+        int ctr=0 ;
+        for (MethodInfo method : methods)
+            createMethod( ctr++, method ) ;
+
+        _end() ; // of _class
+
+        return _generate( anchorClass, debug ? debugProps : emptyProps, ps ) ;
     }
 
     private static final Type objectArrayType = Type._array(_Object()) ;
