79bb0e57ed54450736da9d95f102bc43ed65f173
Lukas Jungmann <lukas.jungmann@oracle.com>
2020-12-05 00:25:49 +0100

    #101: fix license in source files
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- orbmain/src/main/java/com/sun/corba/ee/impl/util/RepositoryId.java
+++ orbmain/src/main/java/com/sun/corba/ee/impl/util/RepositoryId.java
@@ -1,13 +1,22 @@
 /*
- * Copyright (c) 1997, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2020 Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 1998-1999 IBM Corp. All rights reserved.
  * Copyright (c) 2019-2020 Payara Services Ltd.
  *
  * This program and the accompanying materials are made available under the
- * terms of the Eclipse Distribution License v. 1.0, which is available at
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the Eclipse Distribution License
+ * v. 1.0 which is available at
  * http://www.eclipse.org/org/documents/edl-v10.php.
  *
- * SPDX-License-Identifier: BSD-3-Clause
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License v2.0
+ * w/Classpath exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR BSD-3-Clause OR GPL-2.0 WITH
+ * Classpath-exception-2.0
  */
 
 package com.sun.corba.ee.impl.util;
